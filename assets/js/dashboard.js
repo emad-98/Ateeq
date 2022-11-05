c01e34cd44602ec7ad2dcc95f46ceb76771f9680
emad-98 <62222805+emad-98@users.noreply.github.com>
2022-11-05 14:37:19 +0200

    Add files via upload

---
+++ assets/js/dashboard.js
@@ -0,0 +1,17 @@
+﻿/*document.querySelector('#homeNav').addEventListener('click', () => {
+    window.location.replace("/Member/Dashboard")
+    if (!document.getElementById("kt_body").hasAttribute("data-kt-aside-minimize")) {
+        document.querySelector('#arrowLeftSide').click();
+    }
+
+
+})*/
+document.querySelectorAll('.navSide').forEach(e => {
+    e.addEventListener('click', () => {
+        if (document.getElementById("kt_body").hasAttribute("data-kt-aside-minimize")) {
+            document.querySelector('#arrowLeftSide').click();
+
+        }
+    })
+}) 
+  
